fca90dcd86189d52956421918108ebf654b20338
lurym <lurym89@gmail.com>
2016-05-11 22:16:10 +0200

    Styles added + fixed encoding + express.js

--- index.js
+++ index.js
@@ -1,53 +1,79 @@
-var DOM_URL = 'http://www.ztm.gda.pl/rozklady/pobierz_SIP.php?n[0]=1371&t=&l=210';
-var PRACA_URL = 'http://www.ztm.gda.pl/rozklady/pobierz_SIP.php?n[0]=1404&t=&l=210';
-
 var http = require('http');
+var express = require('express');
+var iconv = require('iconv-lite');
 
-var server = http.createServer();
+/* SERVER INITIALIZATION */
+var app = express();
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 1180;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 1180
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
-
-server
-  .on('request', onRequest)
-  .listen(server_port, server_ip_address, function () {
+app.use(express.static(__dirname + '/public'));
+app.get('/', onRequest);
+app.listen(server_port, server_ip_address, function () {
     console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
 });
 
-function makeSingleRequest(url, res, callback) {
+/* CONSTANTS */
+var DOM_URL = 'http://www.ztm.gda.pl/rozklady/pobierz_SIP.php?n[0]=1371&t=&l=210';
+var PRACA_URL = 'http://www.ztm.gda.pl/rozklady/pobierz_SIP.php?n[0]=1404&t=&l=210';
+var HTML = '<html><head>\
+<link href="./style.css" rel="stylesheet" type="text/css">\
+</head><body>{0}</body></html>';
+
+/* UTILS*/
+String.format = function(format) {
+	var args = Array.prototype.slice.call(arguments, 1);
+	return format.replace(/{(\d+)}/g, function(match, number) { 
+		return typeof args[number] != 'undefined'
+			? args[number] 
+			: match;
+	});
+};
+
+function reduceResponses(res, responses) {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+	output = String.format(HTML, responses.join('</div>'));
+    console.log("Request handled successfully");
+	res.end(output);
+}
+
+function makeSingleRequest(url, callback) {
     console.log(url);
     http.get(url, function (resp) {
         var chunks = [];
         resp.on('data', function(chunk) {
-            chunks.push(chunk);
+			decodedStr = iconv.decode(chunk, 'iso-8859-2');
+            chunks.push(decodedStr);
         });
         resp.on('end', function() {
-            res.write(Buffer.concat(chunks));
-            callback();
+            singleResponse = chunks.join();
+            callback(singleResponse);
         });
     });
 }
 
 function makeMultipleRequests(urls, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=iso-8859-2' });
     var requestsFinished = 0;
+    var responses = [];
     urls.forEach(function(url) {
-        makeSingleRequest(url, res, function() {
+        makeSingleRequest(url, function(singleResponse) {
+            responses.push(singleResponse);
             requestsFinished++;
             if (requestsFinished == urls.length) {
-                console.log("Request handled successfully");
-                res.end();
+                reduceResponses(res, responses);
              }
         });
     });
 }
 
+/* REQUEST HANDLER */
 function onRequest(req, res) {
   console.log('Request retrieved ' + req.url);
   if (req.url === '/') {
     makeMultipleRequests([DOM_URL, PRACA_URL], res);
   }
   else {
+	res.writeHead(404);
     res.end();
   }
 }
